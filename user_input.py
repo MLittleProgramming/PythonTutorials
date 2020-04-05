cf57a3a494ed27d54d0cd2190c12d64e28ea9ba1
MLittleProgramming <MLittleProgramming@gmail.com>
2020-04-06 00:07:40 +0100

    Add User Input Code

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# User Input in Python
+# Video Link:https://youtu.be/EKByWqqsNC8
+
+user_input = input("enter a character: ")[0]  # input() allows user to enter input
+user_input2 = input("enter a second character: ")[0]  # [0] means we only take the first character from the users input
+manipulate_input = user_input + user_input2  # concatenate user input
+print(manipulate_input)  # print user input
+
+# MLittleProgramming on Youtube: https://www.youtube.com/channel/UC7-MzZxV8JJElJknyDJJKcQ
